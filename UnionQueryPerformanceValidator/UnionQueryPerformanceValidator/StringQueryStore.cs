2485ec611d4a8d26441123a8fe94c8d482e5c85e
Vikram Rakhe <vikram.rakhe@siemens.com>
2016-10-27 20:01:26 +0530

    PLINQ compare with 500000 records in filter

--- UnionQueryPerformanceValidator/UnionQueryPerformanceValidator/StringQueryStore.cs
+++ UnionQueryPerformanceValidator/UnionQueryPerformanceValidator/StringQueryStore.cs
@@ -5,15 +5,15 @@ namespace UnionQueryPerformanceValidator
     public class StringQueryStore
     {
         private static readonly string SingleUnionQuery =
-            "select PUID from (select PUID from pitem where rownum < 100000 UNION ALL select PUID from pitemrevision where rownum < 100000 UNION ALL select PUID from pworkspaceobject where rownum < 100000 UNION ALL select PUID from ppom_application_object where rownum < 100000 UNION ALL select PUID from PImanRelation where rownum < 100000 ) order by PUID";
+            "select PUID from (select PUID from pitem where rownum < 500000 UNION ALL select PUID from pitemrevision where rownum < 500000 UNION ALL select PUID from pworkspaceobject where rownum < 500000 UNION ALL select PUID from ppom_application_object where rownum < 500000 UNION ALL select PUID from PImanRelation where rownum < 500000 ) order by PUID";
 
         private static readonly string[] MultiQueriesStore =
         {
-            "select PUID from pitem where rownum < 100000 order by PUID",
-            "select PUID from pitemrevision where rownum< 100000 order by PUID",
-            "select PUID from pworkspaceobject where rownum < 100000 order by PUID",
-            "select PUID from ppom_application_object where rownum< 100000 order by PUID",
-            "select PUID from PImanRelation where rownum < 100000 order by PUID"
+            "select PUID from pitem where rownum < 500000",
+            "select PUID from pitemrevision where rownum< 500000",
+            "select PUID from pworkspaceobject where rownum < 500000",
+            "select PUID from ppom_application_object where rownum< 500000",
+            "select PUID from PImanRelation where rownum < 500000"
         };
 
         public string ReadSingleUnionQuery()
