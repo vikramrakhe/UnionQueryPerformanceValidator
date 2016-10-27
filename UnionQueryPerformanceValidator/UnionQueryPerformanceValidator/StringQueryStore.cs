33095fb2fd05e82195ed108bb588e37e36c5545c
Vikram Rakhe <vikram.rakhe@siemens.com>
2016-10-27 19:55:33 +0530

    Added Order by to all parallel queries to see the result.

--- UnionQueryPerformanceValidator/UnionQueryPerformanceValidator/StringQueryStore.cs
+++ UnionQueryPerformanceValidator/UnionQueryPerformanceValidator/StringQueryStore.cs
@@ -9,11 +9,11 @@ namespace UnionQueryPerformanceValidator
 
         private static readonly string[] MultiQueriesStore =
         {
-            "select PUID from pitem where rownum < 100000",
-            "select PUID from pitemrevision where rownum< 100000",
-            "select PUID from pworkspaceobject where rownum < 100000",
-            "select PUID from ppom_application_object where rownum< 100000",
-            "select PUID from PImanRelation where rownum < 100000"
+            "select PUID from pitem where rownum < 100000 order by PUID",
+            "select PUID from pitemrevision where rownum< 100000 order by PUID",
+            "select PUID from pworkspaceobject where rownum < 100000 order by PUID",
+            "select PUID from ppom_application_object where rownum< 100000 order by PUID",
+            "select PUID from PImanRelation where rownum < 100000 order by PUID"
         };
 
         public string ReadSingleUnionQuery()
